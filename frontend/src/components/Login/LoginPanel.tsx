6386cb71560bdf9c9e754a4268d6b386b5ddc059
ObedDM <pacocobot2@gmail.com>
2025-04-16 23:40:46 -0600

    Added credentials retrieval functionality

--- frontend/src/components/Login/LoginPanel.tsx
+++ frontend/src/components/Login/LoginPanel.tsx
@@ -1,4 +1,5 @@
-import GradientButton from "../customButtons/gradientButton";
+import { useState } from "react"
+import GradientButton from "../customButtons/GradientButton"
 
 interface LoginPanelProps {
     buttonLogIn: string[]
@@ -6,20 +7,47 @@ interface LoginPanelProps {
 }
 
 export default function LoginPanel({ buttonLogIn, buttonCreateAccount }: LoginPanelProps) {
+    
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin() {
+        console.log('Username:', username);
+        console.log('Password:', password);
+    }
+
+    function getUsername(e: React.ChangeEvent<HTMLInputElement>) {
+        setUsername(e.target.value);
+    }
+
+    function getPassword(e: React.ChangeEvent<HTMLInputElement>) {
+        setPassword(e.target.value);
+    }
+    
     return (
         <div className="absolute top-1/2 right-1/8 transform -translate-y-1/2">
             <div className="bg-white w-[510px] h-[540px] px-15 py-11 rounded-[20px] shadow-[0_5px_20px_0_rgba(0,0,0,0.25)]">
                 <h1 className="font-medium text-black text-[50px] mb-4"> Log in </h1>
                 
                 <label className="text-[20px]"> Username </label>
-                <input className="text-[20px] border rounded-[5px] px-1 pb-1 mt-1 mb-4 w-full focus:outline-none focus:ring-[1px] focus:ring-black shadow-[0_2px_4px_0_rgba(0,0,0,.15)]"></input>
+                <input
+                    type="text"
+                    className="text-[20px] border rounded-[5px] px-1 pb-1 mt-1 mb-4 w-full focus:outline-none focus:ring-[1px] focus:ring-black shadow-[0_2px_4px_0_rgba(0,0,0,.15)]"
+                    onChange={getUsername}/>
 
                 <label className="text-[20px]"> Password </label>
-                <input type="password" className="text-[20px] border rounded-[5px] px-1 pb-1 mt-1 mb-1 w-full focus:outline-none focus:ring-[1px] focus:ring-black shadow-[0_2px_4px_0_rgba(0,0,0,.15)]"></input>
-                <a href="#" className="text-[17px] text-[#A0A0A0] hover:text-[#808080] hover:underline">Forgot your password?</a>
+                <input
+                    type="password"
+                    className="text-[20px] border rounded-[5px] px-1 pb-1 mt-1 mb-1 w-full focus:outline-none focus:ring-[1px] focus:ring-black shadow-[0_2px_4px_0_rgba(0,0,0,.15)]"
+                    onChange={getPassword}/>
+                
+                <a className="text-[17px] text-[#A0A0A0] hover:text-[#808080] hover:underline">Forgot your password?</a>
             
                 {/* Log in button */}
-                <GradientButton text={buttonLogIn[0]} gradientClass={buttonLogIn[1]}/>
+                <GradientButton
+                    text={buttonLogIn[0]}
+                    gradientClass={buttonLogIn[1]}
+                    onClick={handleLogin}/>
 
                 <div className="flex items-center mt-4">
                     <hr className="grow border-[#A0A0A0]" />
@@ -29,7 +57,10 @@ export default function LoginPanel({ buttonLogIn, buttonCreateAccount }: LoginPa
 
                 {/* Create Account button */}
                 <div className="flex justify-center mt-4">
-                    <GradientButton text={buttonCreateAccount[0]} gradientClass={buttonCreateAccount[1]}/>
+                    <GradientButton
+                        text={buttonCreateAccount[0]}
+                        gradientClass={buttonCreateAccount[1]}
+                        onClick={() => {}}/>
                 </div>
             </div>
         </div>
--- frontend/src/components/customButtons/GradientButton.tsx
+++ frontend/src/components/customButtons/GradientButton.tsx
@@ -1,13 +1,16 @@
 interface GradientButtonProps {
     text: string;
     gradientClass: string
+    onClick: () => void;
 }
 
-export default function GradientButton({ text, gradientClass }: GradientButtonProps) {
+export default function GradientButton({ text, gradientClass, onClick }: GradientButtonProps) {
     return (
         <div className="flex justify-end">
-            <button className={ `${gradientClass} glow-on-hover text-[18px] text-white tracking-wide pb-[6px] pt-1 px-6 rounded-[20px] shadow-[0_2px_6px_0_rgba(0,0,0,.25)] transition-all duration-300 font-bold w-auto cursor-pointer` }>
-                {text}
+            <button
+                className={ `${gradientClass} glow-on-hover text-[18px] text-white tracking-wide pb-[6px] pt-1 px-6 rounded-[20px] shadow-[0_2px_6px_0_rgba(0,0,0,.25)] transition-all duration-300 font-bold w-auto cursor-pointer` }
+                onClick={onClick}>
+                    {text}
             </button>
         </div>
     )
