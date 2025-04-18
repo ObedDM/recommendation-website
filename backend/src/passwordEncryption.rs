4a1aae9c4262b2c3e1b021728c87a1b8f948a24a
ObedDM <pacocobot2@gmail.com>
2025-04-17 20:18:16 -0600

    Added encrypted password generation

---
+++ backend/src/passwordEncryption.rs
@@ -0,0 +1,10 @@
+use bcrypt::hash;
+
+pub fn generate_password(password: &str) -> Result<String, String> {
+    const COST: u32 = 5;
+
+    match hash(password, COST) {
+        Ok(hashed_password) => Ok(hashed_password),
+        Err(e) => Err(format!("Failed to hash password: {}", e))
+    }
+}
