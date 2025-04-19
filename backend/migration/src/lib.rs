30588bd26e1c72bf6af98fc43e01e6626b233291
ObedDM <pacocobot2@gmail.com>
2025-04-19 13:51:12 -0600

    Added UUID column in User (m20250419_192352_add_uuid_pk_User) migration

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
+mod m20250419_192352_add_uuid_pk_User;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_create_table::Migration)]
+        vec![
+            Box::new(m20220101_000001_create_table::Migration),
+            Box::new(m20250419_192352_add_uuid_pk_User::Migration),
+        ]
     }
 }
