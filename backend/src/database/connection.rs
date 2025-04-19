69679df9cb3e9212cf29174948ffb060cf72d14e
ObedDM <pacocobot2@gmail.com>
2025-04-19 01:08:56 -0600

    Fixed url

--- backend/src/database/connection.rs
+++ backend/src/database/connection.rs
@@ -4,7 +4,7 @@ use std::env::var;
 
 pub async fn pg_connection() -> DatabaseConnection {
     dotenv().expect("Cannot access .env file");
-    let DATABASE_URL = var("SERVER_ADDRESS").expect("Database URL was not set in the environment");
+    let DATABASE_URL = var("DATABASE_URL").expect("Database URL was not set in the environment");
 
     let db: DatabaseConnection = Database::connect(DATABASE_URL).await.expect("Cannot connect to database");
 
