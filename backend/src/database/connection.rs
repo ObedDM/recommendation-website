97745cf3262bbddb6ac0a3501fad848de7958a2e
ObedDM <pacocobot2@gmail.com>
2025-04-19 00:17:02 -0600

    Set up database connection

---
+++ backend/src/database/connection.rs
@@ -0,0 +1,12 @@
+use sea_orm::{Database, DatabaseConnection};
+use dotenvy::dotenv;
+use std::env::var;
+
+pub async fn pg_connection() -> DatabaseConnection {
+    dotenv().expect("Cannot access .env file");
+    let DATABASE_URL = var("SERVER_ADDRESS").expect("Database URL was not set in the environment");
+
+    let db: DatabaseConnection = Database::connect(DATABASE_URL).await.expect("Cannot connect to database");
+
+    return db
+}
