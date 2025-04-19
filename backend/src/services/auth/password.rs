08281eca42104d0bc5ee01a5ed33eecaba4d77d5
ObedDM <pacocobot2@gmail.com>
2025-04-18 20:37:59 -0600

    Created password authentication

--- backend/src/services/auth/password.rs
+++ backend/src/services/auth/password.rs
@@ -1,7 +1,7 @@
-use bcrypt::hash;
+use bcrypt::{hash, verify};
 
 pub fn generate_password(password: &str) -> Result<String, String> {
-    const COST: u32 = 5;
+    const COST: u32 = 13;
 
     match hash(password, COST) {
         Ok(hashed_password) => Ok(hashed_password),
@@ -9,6 +9,10 @@ pub fn generate_password(password: &str) -> Result<String, String> {
     }
 }
 
-pub fn check_password(password: &str) -> Result<String, String> {
-    todo()
+pub fn check_password(password: &str, hash: &str) -> Result<bool, String> {
+
+    match verify(password, hash) {
+        Ok(is_password) => Ok(is_password),
+        Err(e) => Err(format!("Failed to verify password: {}", e))
+    }
 }
