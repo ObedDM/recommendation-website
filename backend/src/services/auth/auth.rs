dcdc055f25644a28f738bee8abe46716fa1ebfb1
ObedDM <pacocobot2@gmail.com>
2025-04-19 15:25:42 -0600

    Deleted register_user function

--- backend/src/services/auth/auth.rs
+++ backend/src/services/auth/auth.rs
@@ -29,26 +29,3 @@ pub async fn create_user(user: UserCredentials) -> Result<String, AuthError> {
 
     Ok("User created succesfully".to_string())
 }
-
-
-
-
-
-
-pub async fn register_user(user: Json<UserCredentials>) -> Result<(StatusCode, String), (StatusCode, String)> {
-
-    
-
-
-    Ok((
-        StatusCode::OK,
-        json!( {"message": "cruella"} ).to_string()
-    ))
-
-    /*
-    1.- Get user information (via JSON)
-    2.- Verify if the username, email dont exist in the database (return err)
-    3.- call passwordEncryption::generate_password() to create a hash for the password
-    4.- insert user, email, password into database (return Ok)
-    */
-}
