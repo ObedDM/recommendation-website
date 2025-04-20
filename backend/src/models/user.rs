95cb75853b1fc61c94cb7f6396dcacbb2fcecf67
ObedDM <pacocobot2@gmail.com>
2025-04-20 04:12:42 -0600

    Created JSON input model for login data

--- backend/src/models/user.rs
+++ backend/src/models/user.rs
@@ -1,9 +1,15 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
-pub struct UserCredentials {
+pub struct SignupCredentials {
     pub username: String,
     pub email: String,
     pub password: String,
     pub country: String
+}
+
+#[derive(Deserialize)]
+pub struct LoginCredentials {
+    pub username: String,
+    pub password: String
 }
