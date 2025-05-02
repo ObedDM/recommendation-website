a4b0bdbedd870466e555b235681b875c75bb4240
ObedDM <pacocobot2@gmail.com>
2025-05-02 17:25:59 -0600

    Implemented /gethome endpoint

--- frontend/src/routes/Home/HomePath.tsx
+++ frontend/src/routes/Home/HomePath.tsx
@@ -1,7 +1,37 @@
 export default function HomePath() {
+
+    async function getHome() {
+        try {
+            const response = await fetch("http://localhost:5050/gethome", {
+                method: "GET",
+                credentials: "include"
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                console.log(result)
+
+                return true
+
+            } else {
+                console.warn(result.message)
+
+                return false
+            }
+        } catch (err) {
+            console.error("Error getting /Home info", err)
+        }
+    }
+
     return (
         <div className="bg-white">
             <h1 className="text-blue-500"> Welcome Home </h1>
+
+            <button onClick={getHome}>
+                ee
+            </button>
+
         </div>
     )
 }
