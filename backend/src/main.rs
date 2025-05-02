53412802e7a7f70f060193ec7fd50a5d65b1431e
ObedDM <pacocobot2@gmail.com>
2025-05-02 17:08:28 -0600

    Added .env initializer for global scope

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,19 @@
 use env_logger;
 use sea_orm::DatabaseConnection;
+use dotenvy::dotenv;
 use crate::database::connection::pg_connection;
 mod server;
 mod models;
 mod routes;
 mod services;
 mod database;
+mod utils;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     env_logger::init();
+
     let db: DatabaseConnection = pg_connection().await;
 
     server::run_server(db).await; 
