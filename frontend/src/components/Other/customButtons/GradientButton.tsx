693a1d49349af8dc9838a2952da2819d921a7885
ObedDM <pacocobot2@gmail.com>
2025-05-02 17:28:18 -0600

    Added support for button-routing-<Link> inside custom buttons

--- frontend/src/components/Other/customButtons/GradientButton.tsx
+++ frontend/src/components/Other/customButtons/GradientButton.tsx
@@ -1,17 +1,26 @@
+import { Link } from "react-router-dom";
+
 interface GradientButtonProps {
     text: string;
-    gradientClass: string
+    gradientClass: string;
+    route: string | null;
     onClick: () => void;
 }
 
-export default function GradientButton({ text, gradientClass, onClick }: GradientButtonProps) {
+export default function GradientButton({ text, gradientClass, route, onClick, }: GradientButtonProps) {
+    const button = (
+        <button
+            className={`${gradientClass} glow-on-hover text-[18px] text-white tracking-wide pb-[6px] pt-1 px-6 rounded-[20px] shadow-[0_2px_6px_0_rgba(0,0,0,.25)] transition-all duration-300 font-bold w-auto cursor-pointer`}
+            onClick={onClick}>
+                {text}
+        </button>
+    );
+
+
+    
     return (
         <div className="flex justify-end">
-            <button
-                className={ `${gradientClass} glow-on-hover text-[18px] text-white tracking-wide pb-[6px] pt-1 px-6 rounded-[20px] shadow-[0_2px_6px_0_rgba(0,0,0,.25)] transition-all duration-300 font-bold w-auto cursor-pointer` }
-                onClick={onClick}>
-                    {text}
-            </button>
+            {route ? <Link to={`${route}`}> {button} </Link> : button}
         </div>
     )
 }
