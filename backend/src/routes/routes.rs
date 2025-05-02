041d83b19f47dd7baf9624cbd0b86227a1c62658
ObedDM <pacocobot2@gmail.com>
2025-05-02 17:22:12 -0600

    Added protected/public routing and changed CORS settings to allow for cookies

--- backend/src/routes/routes.rs
+++ backend/src/routes/routes.rs
@@ -1,19 +1,30 @@
-use axum::{routing::{get, post}, Router};
+use std::env::var;
+
+use axum::{middleware, routing::{get, post}, Extension, Router};
 use sea_orm::DatabaseConnection;
 use tower_http::cors::{CorsLayer, Any};
-use http::{Method, header};
-use super::auth;
+use http::{header::{AUTHORIZATION, CONTENT_TYPE}, HeaderValue, Method};
+use super::{auth, home};
 
 pub fn create_router(db: DatabaseConnection) -> Router {
+    let CLIENT_ADDRESS = var("CLIENT_ADDRESS").expect("Client address was not set in the environment");
 
     let cors_layer = CorsLayer::new()
-        .allow_origin(Any)
+        .allow_origin(CLIENT_ADDRESS.parse::<HeaderValue>().unwrap())
+        .allow_credentials(true)
         .allow_methods(vec![Method::GET, Method::POST])
-        .allow_headers([header::CONTENT_TYPE]);
+        .allow_headers([AUTHORIZATION, CONTENT_TYPE]);
+
+    let protected_routes = Router::new()
+        .route("/gethome", get(home::home));
+
+    let public_routes = Router::new()
+        .route("/auth/signup", post(auth::signup))
+        .route("/auth/login", post(auth::login));
 
     Router::new()
-        .route("/auth/signup", post(auth::signup))
-        .route("/auth/login", post(auth::login))
+        .merge(protected_routes)
+        .merge(public_routes)
         .layer(cors_layer)
-        .with_state(db)
+        .with_state(db) 
 }
