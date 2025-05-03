855654f9c8619d8f8dfd8f09e861010e72180bef
ObedDM <pacocobot2@gmail.com>
2025-05-03 04:57:11 -0600

    Created ProfilePath as a placeholder for /profile route

---
+++ frontend/src/routes/Profile/ProfilePath.tsx
@@ -0,0 +1,7 @@
+export default function ProfilePath() {
+    return (
+        <div className="bg-amber-600">
+            <h1 className="text-2xl"> Hello! </h1>
+        </div>
+    )
+}
--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -3,11 +3,13 @@ import App from "../App";
 import AuthPath from "./Auth/AuthPath";
 import HomePath from "./Home/HomePath";
 import NotFound from "../components/NotFound/NotFound";
+import ProfilePath from "./Profile/ProfilePath";
 
 const router = createBrowserRouter([
     {path: "/", element: <App />},
     {path: "/home", element: <HomePath />},
     {path: "/auth", element: <AuthPath />},
+    {path: "/profile", element: <ProfilePath />},
     {path: "*", element: <NotFound />},
   ]);
 
