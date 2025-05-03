f92db281113a8c0e31909999b0781bcb45a22176
ObedDM <pacocobot2@gmail.com>
2025-05-03 04:57:49 -0600

    Fixed route link to go to root instead of home

--- frontend/src/components/NotFound/NotFound.tsx
+++ frontend/src/components/NotFound/NotFound.tsx
@@ -4,8 +4,8 @@ export default function NotFound() {
     return (
         <div className="bg-white">
             <h1 className="text-pink-500"> This page does not exist! </h1>
-            <Link to={"/home"}>
-                <button className="border rounded-md p-1 text-blue-500"> Go back home </button>
+            <Link to={"/"}>
+                <button className="border rounded-md p-1 text-blue-500"> Go back </button>
             </Link>
         </div>
     );
