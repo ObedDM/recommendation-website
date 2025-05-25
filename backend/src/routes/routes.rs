7b729eb5d7df458ce9591c48370dbf1e575cfed3
ObedDM <pacocobot2@gmail.com>
2025-05-24 21:49:02 -0600

    Updated route names for /gethome to /home; /getprofile to /profile
    Added handler for DELETE profile
    
    Added endpoint for updating profile picture /profile/picture

--- backend/src/routes/routes.rs
+++ backend/src/routes/routes.rs
@@ -1,8 +1,8 @@
 use std::env::var;
 
-use axum::{middleware, routing::{get, post}, Extension, Router};
+use axum::{routing::{get, post, put, delete}, Router};
 use sea_orm::DatabaseConnection;
-use tower_http::cors::{CorsLayer, Any};
+use tower_http::cors::{CorsLayer};
 use http::{header::{AUTHORIZATION, CONTENT_TYPE}, HeaderValue, Method};
 use super::{auth, home, refresh, profile};
 
@@ -12,12 +12,15 @@ pub fn create_router(db: DatabaseConnection) -> Router {
     let cors_layer = CorsLayer::new()
         .allow_origin(CLIENT_ADDRESS.parse::<HeaderValue>().unwrap())
         .allow_credentials(true)
-        .allow_methods(vec![Method::GET, Method::POST])
+        .allow_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE])
         .allow_headers([AUTHORIZATION, CONTENT_TYPE]);
 
     let protected_routes = Router::new()
-        .route("/gethome", get(home::home))
-        .route("/getprofile", get(profile::profile))
+        .route("/home", get(home::home))
+        .route("/profile", get(profile::get_profile).delete(profile::delete))
+        //.route("/profile/picture/post", post(profile::post_picture))
+        .route("/profile/picture", put(profile::put_picture))
+        //.route("/profile/picture/refresh", get(handler))
         .route("/auth/refresh", post(refresh::refresh));
 
     let public_routes = Router::new()
