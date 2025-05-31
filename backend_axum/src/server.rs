e0ddb1c3fbb2b1da5bfc6616f5205e6df0e74dca
ObedDM <pacocobot2@gmail.com>
2025-05-31 11:12:05 -0600

    LIux swap shutdown setup

--- backend_axum/src/server.rs
+++ backend_axum/src/server.rs
@@ -1,5 +1,5 @@
 use sea_orm::DatabaseConnection;
-use tokio::{net::TcpListener, signal, sync::oneshot};
+use tokio::{net::TcpListener, signal::unix::{signal, SignalKind}, sync::oneshot};
 use dotenvy::dotenv;
 use std::env::var;
 use axum::{serve, Router};
@@ -12,19 +12,19 @@ pub async fn run_server(db: DatabaseConnection) {
     let SERVER_ADDRESS = var("SERVER_ADDRESS").unwrap_or("127.0.0.1:3000".to_string());
 
     // Shutdown setup [Linux]
-    /*let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
+    let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
     tokio::spawn(async move {
         let mut sigint = signal(SignalKind::interrupt()).unwrap();
         sigint.recv().await;
         let _ = shutdown_tx.send(());
-    });*/
+    });
 
     // Shutdown setup [Windows]
-    let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
+    /*let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
     tokio::spawn(async move {
         signal::ctrl_c().await.expect("failed to listen for ctrl_c");
         let _ = shutdown_tx.send(());
-    });
+    });*/
 
     // TcpListener
     let listener = TcpListener::bind(SERVER_ADDRESS).await.expect("Could not create TcpListener");
